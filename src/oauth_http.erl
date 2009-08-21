0bbd3210bc6f60fcf50a233a98346856ddc9d97e
Christian Westbrook <cw@cotweet.com>
2009-08-21 15:09:18 -0700

    Altering this teeny 'fork' of erlang-oauth to use instead lhttpc.

--- src/oauth_http.erl
+++ src/oauth_http.erl
@@ -2,15 +2,25 @@
 
 -export([get/1, post/2, response_params/1, response_body/1, response_code/1]).
 
+-define(TIMEOUT_MS, 6000).
+-define(HTTP_REQ_HEADERS, [{"User-Agent", "CoTweetRobot/0.0.2"}]).
 
 get(URL) ->
-  request(get, {URL, []}).
+  request("GET", {URL, []}).
 
 post(URL, Data) ->
-  request(post, {URL, [], "application/x-www-form-urlencoded", Data}).
+  request("POST", {URL, [], "application/x-www-form-urlencoded", Data}).
 
-request(Method, Request) ->
-  http:request(Method, Request, [{autoredirect, false}], []).
+% inets http client version.
+%request(Method,Request) ->
+%   http:request(Method, Request, [{autoredirect, false}], []).
+
+request(Method, {URL, _} = _Req) when is_list(Method) ->
+    lhttpc:request(URL, Method, [], ?TIMEOUT_MS);
+
+request(Method, {URL, Args, ContentType, PostData} = _Req) when is_list(Method) ->
+    Hdrs = lists:append(?HTTP_REQ_HEADERS, [{"Content-Type",ContentType}]),
+    lhttpc:request(URL, Method, Hdrs, PostData, ?TIMEOUT_MS).
 
 response_params(Response) ->
   oauth_uri:params_from_string(response_body(Response)).
